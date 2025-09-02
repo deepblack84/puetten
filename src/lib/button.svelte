cb817cb58d2b179b017c0d58cd519756decb0b46
Ole <ole.ostermann@googlemail.com>
2025-09-02 20:42:16 +0200

    change captain script

---
+++ src/lib/button.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-wrap justify-center gap-6">
+    <a class="relative" href="#">
+        <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+        <span class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-jomi hover:text-gray-900">Informationen</span>
+    </a>
+</div>
